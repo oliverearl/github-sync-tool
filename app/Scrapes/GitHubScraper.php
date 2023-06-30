c5ca14564879c17d144efb96813c45275057f6b8
Oliver Earl <oliver@oliverearl.co.uk>
2023-06-30 14:26:41 +0100

    fix: GitHub scraper updated for the new GitHub interface.

--- app/Scrapes/GitHubScraper.php
+++ app/Scrapes/GitHubScraper.php
@@ -6,11 +6,24 @@ namespace App\Scrapes;
 
 use App\Objects\ContributionSquare;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 use Symfony\Component\DomCrawler\Crawler;
 
 class GitHubScraper
 {
-    public const GITHUB_URL = 'https://www.github.com/';
+    /**
+     * The GitHub URL used for the scraper.
+     *
+     * @var string
+     */
+    final public const GITHUB_URL = 'https://www.github.com/';
+
+    /**
+     * The tab name used for harvesting data.
+     *
+     * @var string
+     */
+    final public const TAB_NAME = 'overview';
 
     /**
      * Scrape a given GitHub profile.
@@ -18,7 +31,7 @@ class GitHubScraper
      * @param string $username
      * @param string $year
      *
-     * @return array
+     * @return array<int, \App\Objects\ContributionSquare>
      */
     public function scrape(string $username, string $year): array
     {
@@ -27,31 +40,28 @@ class GitHubScraper
 
         /** @var \Illuminate\Http\Client\Response $response */
         $response = Http::throw()->get($url, [
-            'tab' => 'overview',
+            'tab' => self::TAB_NAME,
             'from' => "{$year}-01-01",
             'to' => "{$year}-12-31",
         ]);
 
         $contributionGraph = app(Crawler::class, ['node' => $response->body(), 'uri' => $url])
-            ->filter('svg')
-            ->filter('.js-calendar-graph-svg')
-            ->children('g')
-            ->first()
-            ->children('g')
-            ->children('rect');
+            ->filter('.ContributionCalendar-day');
 
-        $contributionGraph->each(static function (Crawler $square) use (&$graph) {
-            $count = $square->attr('data-count');
+        $contributionGraph->each(static function (Crawler $square) use (&$graph): void {
             $date = $square->attr('data-date');
+            $hasData = $square->attr('data-level');
 
-            if ($count > 0) {
-                $graph[] = new ContributionSquare(
-                    date: $date,
-                    count: (int) $count,
-                );
+            if ($hasData !== '0') {
+                $contributionCount = (int) Str::before($square->innerText(), ' contributions');
+
+                if ($contributionCount > 0) {
+                    $graph[] = new ContributionSquare(
+                        date: $date,
+                        count: $contributionCount,
+                    );
+                }
             }
         });
-
-        return $graph;
     }
 }
